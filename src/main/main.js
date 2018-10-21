5794ffa3f6bfd004ad83b9f6f95380599c405ff3
Kerolos Zaki <kerolos4zaki@gmail.com>
2018-10-21 06:09:27 +0200

    the dawayer icon and tray icon

--- src/main/main.js
+++ src/main/main.js
@@ -4,6 +4,7 @@ import path from 'path';
 import url from 'url';
 
 import { setWindow, setApp, reload, focus, quit } from './window.js';
+import { loadOptions } from './options.js';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -82,6 +83,8 @@ function createWindow()
 
   setWindow(mainWindow);
   setApp(app);
+
+  loadOptions();
 
   // and load the index.html of the app
   mainWindow.loadURL(url.format({
---
+++ src/main/options.js
@@ -0,0 +1,57 @@
+import { Menu, Tray, app, nativeImage } from 'electron';
+
+import { join } from 'path';
+
+import * as settings from 'electron-json-config';
+
+/** @type { Tray }
+*/
+export let trayIcon;
+
+const trayMenuTemplate = [
+  {
+    label: 'Dawayer',
+    enabled: false
+  },
+  {
+    type: 'separator'
+  },
+  {
+    label: 'Quit', click()
+    {
+      app.quit();
+    }
+  }
+];
+
+export function loadOptions()
+{
+  loadTrayIcon();
+}
+
+function loadTrayIcon()
+{
+  if (trayIcon)
+    return;
+
+  const enabled = settings.get('trayIcon', true);
+
+  if (enabled)
+  {
+    const trayMenu = Menu.buildFromTemplate(trayMenuTemplate);
+    const trayIconImage = nativeImage.createFromPath(join(__dirname, '../../tray.png'));
+  
+    trayIcon = new Tray(trayIconImage);
+
+    trayIcon._setContextMenu = trayIcon.setContextMenu;
+
+    trayIcon.setContextMenu = function(menu)
+    {
+      trayIcon._setContextMenu(menu);
+      
+      trayIcon.contextMenu = menu;
+    };
+
+    trayIcon.setContextMenu(trayMenu);
+  }
+}
