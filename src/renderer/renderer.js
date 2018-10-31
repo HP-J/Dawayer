a1ff3ebc83070ad96b668b090a355d2fbc419975
Kerolos Zaki <kerolos4zaki@gmail.com>
2018-10-31 06:26:15 +0100

    tooltips using tippy.js

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,30 +1,13 @@
 // import { remote } from 'electron';
 
 import textFit from 'textfit';
+import tippy from 'tippy.js';
 
 let resizeEndTimeout;
 
 const rewindTimeText = document.body.querySelector('.rewindTime');
 const forwardTimeText = document.body.querySelector('.forwardTime');
-
-window.onload = () =>
-{
-  resizeEnd();
-};
-
-window.onresize = () =>
-{
-  // clear old resize-end timeout event
-  if (resizeEndTimeout)
-    clearTimeout(resizeEndTimeout);
-
-  // add no-motion class
-  if (!document.body.classList.contains('noMotion'))
-    document.body.classList.add('noMotion');
-
-  // set a new resize-end timeout
-  resizeEndTimeout = setTimeout(resizeEnd, 25);
-};
+const volumeButton = document.body.querySelector('.volumeButton');
 
 function resizeEnd()
 {
@@ -45,3 +28,31 @@ function resizeEnd()
   // remove no-motion class
   document.body.classList.remove('noMotion');
 }
+
+function init()
+{
+  tippy.setDefaults({ a11y: false, delay: [ 200, 50 ] });
+
+  tippy(volumeButton, { interactive: true, content: 'Volume' });
+}
+
+window.onload = () =>
+{
+  resizeEnd();
+};
+
+window.onresize = () =>
+{
+  // clear old resize-end timeout event
+  if (resizeEndTimeout)
+    clearTimeout(resizeEndTimeout);
+
+  // add no-motion class
+  if (!document.body.classList.contains('noMotion'))
+    document.body.classList.add('noMotion');
+
+  // set a new resize-end timeout
+  resizeEndTimeout = setTimeout(resizeEnd, 25);
+};
+
+init();
