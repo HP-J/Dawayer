9d31bb5d128c556604e110ed0e7d5ef26c003e3f
Kerolos Zaki <kerolos4zaki@gmail.com>
2019-06-03 14:37:14 +0200

    podcast element and placeholder

--- src/renderer/podcasts.js
+++ src/renderer/podcasts.js
@@ -1,13 +1,76 @@
-import * as settings from '../settings.js';
-
 import { readFile } from 'fs-extra';
 
 import request from 'request-promise-native';
 import feedParse from 'davefeedread';
 
+import { createElement } from './renderer.js';
+
+const podcastsContainer = document.body.querySelector('.podcasts.container');
+
 export function initPodcasts()
 {
 
+}
+
+function appendPodcastPlaceholder()
+{
+  const placeholderWrapper = createElement('.podcast.wrapper.placeholder');
+
+  const podcastContainer = createElement('.podcast.container');
+
+  const cover = createElement('.podcast.cover');
+  const artist = createElement('.podcast.artist');
+  const title = createElement('.podcast.title');
+
+  const card = createElement('.podcast.card');
+  const details = createElement('.podcast.details');
+
+  placeholderWrapper.appendChild(podcastContainer);
+
+  podcastContainer.appendChild(cover);
+  podcastContainer.appendChild(artist);
+  podcastContainer.appendChild(title);
+
+  podcastContainer.appendChild(card);
+
+  card.appendChild(details);
+
+  podcastsContainer.appendChild(placeholderWrapper);
+
+  return placeholderWrapper;
+}
+
+/** @param { HTMLDivElement } element
+* @param { { picture: string, artist: string, title: string, details: sting } } options
+*/
+function updatePodcastElement(element, options)
+{
+  if (element.classList.contains('placeholder'))
+    element.classList.remove('placeholder');
+
+  if (options.picture)
+  {
+    element.querySelector('.podcast.cover').style.backgroundImage = `url(${options.picture})`;
+  }
+
+  if (options.artist)
+  {
+    element.querySelector('.podcast.artist').innerText = options.artist;
+  }
+
+  if (options.title)
+  {
+    element.querySelector('.podcast.title').innerText = options.title;
+
+    // element.onclick = () => navigation('play-album', options.title);
+
+    // useContextMenu(element, [ options.title ], 'album', element);
+  }
+
+  if (options.details)
+  {
+    element.querySelector('.podcast.details').innerText = options.details;
+  }
 }
 
 /** @param { string } filename
